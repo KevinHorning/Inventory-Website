17a6026dc8be7ae363d8ee0b5c81fb7593ad8cf1
Neal Ruggles <nealruggles1@live.com>
2018-09-10 23:06:33 -0400

    initial commit

---
+++ Atlas2/Controllers/AuthenticationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Atlas2.Controllers
+{
+    [RoutePrefix("api/authentication")]
+    public class AuthenticationController : ApiController
+    {
+        [HttpGet]
+        [Route("")]
+        public async Task<HttpResponseMessage> TestMethod()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, $"WHAT UP WORLD: ");
+        }
+
+        [HttpGet]
+        [Route("two")]
+        [AuthenticationFilter]
+        public async Task<HttpResponseMessage> Testmethod2()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "oh hi");
+        }
+    }
+}
---
+++ Atlas2/Filters/AuthenticationFilter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace Atlas2
+{
+    public class AuthenticationFilter : FilterAttribute, IActionFilter
+    {
+        public async Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+        {
+            HttpActionExecutedContext executedContext = new HttpActionExecutedContext(actionContext, null);
+
+            try
+            {
+                if (!actionContext.Request.Headers.Contains("userToken"))
+                {
+                    executedContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                }
+                else
+                {
+                    var tokenString = actionContext.Request.Headers.GetValues("userToken").FirstOrDefault();
+
+                    //TODO check database if token is valid
+                    bool isTokenValid = true;
+
+                    if (isTokenValid)
+                    {
+                        //valid token, populate the context with useful information and continue to request
+                        HttpContext.Current.Items["token"] = tokenString;
+                        executedContext.Response = await continuation();
+                    }
+                    else
+                    {
+                        //invalid token, reject request
+                        executedContext.Response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                executedContext = new HttpActionExecutedContext(actionContext, ex);
+            }
+
+            return executedContext.Response;
+        }
+    }
+}
