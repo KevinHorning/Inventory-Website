c735ad48eae9816bd67076f66042e770f4d1c9cd
Neal Ruggles <nealruggles1@live.com>
2018-09-19 14:29:05 -0400

    add mvc views

---
+++ Atlas2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Atlas2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Atlas2/Controllers/DefaultController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Atlas2.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
