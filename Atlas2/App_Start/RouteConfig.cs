4488aea40aa852379682d830981a534faca21759
Kevin Nguyen <KevinNguyen@Kevins-MacBook-Pro.local>
2018-09-20 19:57:57 -0400

    summary

--- Atlas2/App_Start/RouteConfig.cs
+++ Atlas2/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Atlas2
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller="Default", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller="LogIn", action = "message", id = UrlParameter.Optional }
             );
         }
     }
---
+++ Atlas2/Controllers/LogInController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net;
+using System.Net.Http;
+using Atlas2.Controllers;
+using Atlas2.Models;
+using System.Web.Mvc;
+
+namespace Atlas2.Controllers
+{
+    [System.Web.Http.RoutePrefix("api/LogIn")]
+    public class LogInController : ApiController
+    {
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("message")]
+        public HttpResponseMessage GetMessage()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Hello");
+        }
+
+        [System.Web.Http.HttpPost]
+        [System.Web.Http.Route("")]
+        public HttpResponseMessage LogInRequest(Account model)
+        {
+            if (model.Username.Equals("Fake@Example") && model.Password.Equals("Password"))
+                return Request.CreateResponse(HttpStatusCode.OK, "Log In Successful.");
+            else return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Log In Not Successful");
+                }
+
+    }
+}
