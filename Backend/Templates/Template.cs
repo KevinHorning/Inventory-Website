f397021b3af89bc4e5cb2accfdd47633b87b5222
Kevin Horning <kevinsemailaddress123@gmail.com>
2018-11-21 11:11:28 -0500

    template foundation and database edits

---
+++ Backend/Templates/Template.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Templates.TemplateComponents;
+using System;
+using System.Collections.Generic;
+
+namespace Backend.Templates
+{
+    public class Template
+    {
+        int systemTemplateID { get; set; }
+        String name { get; set; }
+
+        List<TemplateComponent> components { get; set; }
+    }
+}
---
+++ Backend/Templates/TemplateComponents/TemplateComponent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Templates.TemplateComponents
+{
+    public class TemplateComponent
+    {
+        int templateComponentID { get; set; }
+        int systemTemplateID { get; set; }
+        int templatePartID { get; set; }
+        int templateSystemID { get; set; }
+        int count{ get; set; }
+    }
+}
